38ffab18f6259358aedf801c022888ffe281698e
August <seanhyg2772689070@gmail.com>
2023-12-08 01:31:58 +0800

    第一个案例，猜数字

--- guessNum/target/debug/demo01.d
+++ guessNum/target/debug/demo01.d
@@ -1,1 +1,1 @@
-E:\BaiduSyncdisk\rust\rust_git\demo01\target\debug\demo01.exe: E:\BaiduSyncdisk\rust\rust_git\demo01\src\main.rs
+E:\BaiduSyncdisk\rust\rust_git\guessNum\target\debug\demo01.exe: E:\BaiduSyncdisk\rust\rust_git\guessNum\src\main.rs
